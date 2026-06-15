97127963b7cfc23da24cc476b13a85edd3566ec2
CompanyProfile <alanood94895@gmail.com>
2026-06-15 10:31:14 +0400

    Create VisaApplication Class

---
+++ src/main/java/com/example/tra/VisaApplication.java
@@ -0,0 +1,26 @@
+package com.example.tra;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class VisaApplication {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Applicant applicant;
+
+    @ManyToOne
+    private ImmigrationOfficer handlingOfficer;
+
+    private String visaType;
+    private String status;
+    private String officerNotes;
+}
