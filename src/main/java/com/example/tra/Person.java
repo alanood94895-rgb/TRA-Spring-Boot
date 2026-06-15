8a8a8506678dffc27e7dae5305c86a2cc5c819f9
CompanyProfile <alanood94895@gmail.com>
2026-06-15 09:44:40 +0400

    implement standard constructors

--- src/main/java/com/example/tra/Person.java
+++ src/main/java/com/example/tra/Person.java
@@ -1,9 +1,14 @@
 package com.example.tra;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
 
 @MappedSuperclass
+@Data
 public class Person {
+    @GeneratedValue(strategy = GenerationType.AUTO)
     String firstName;
     String lastName;
     String gender;
