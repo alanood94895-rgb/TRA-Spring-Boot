3d7228ce76c0750824bd42a43662c4ee87bcdb61
CompanyProfile <alanood94895@gmail.com>
2026-06-15 10:37:39 +0400

    Create interface ApplicantRepository class

---
+++ src/main/java/com/example/tra/Repositories/ApplicantRepository.java
@@ -0,0 +1,11 @@
+package com.example.tra.Repositories;
+
+import com.example.tra.Applicant;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ApplicantRepository extends JpaRepository<Applicant, Long> {
+
+        List<Applicant> findByNationality(String nationality);
+}
