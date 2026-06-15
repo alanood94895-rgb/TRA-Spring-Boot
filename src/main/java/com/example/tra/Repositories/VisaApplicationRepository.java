a6ea79343af7dda0abd8154bc2679d662b6748c7
CompanyProfile <alanood94895@gmail.com>
2026-06-15 10:42:38 +0400

    Create VisaApplicationRepository Class

---
+++ src/main/java/com/example/tra/Repositories/VisaApplicationRepository.java
@@ -0,0 +1,7 @@
+package com.example.tra.Repositories;
+
+import com.example.tra.VisaApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VisaApplicationRepository  extends JpaRepository<VisaApplication, Long> {
+}
