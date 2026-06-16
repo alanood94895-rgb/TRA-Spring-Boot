96556e4c55b456c0f0b2e0ef9a9ce5d398d21660
Alanoud <alanood94895@gmail.com>
2026-06-16 09:47:55 +0400

    Create VisaApplicationService Class

--- src/main/java/com/example/tra/Services/VisaApplicationService.java
+++ src/main/java/com/example/tra/Services/VisaApplicationService.java
@@ -7,6 +7,8 @@ import com.example.tra.Repositories.OfficerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class VisaApplicationService {
     @Autowired
