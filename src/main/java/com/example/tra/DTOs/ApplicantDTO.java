3c519d5a80b41a1d24ab1dd0aea14be68645d38f
Alanoud <alanood94895@gmail.com>
2026-06-17 10:39:14 +0400

    Create ApplicantDTO Class

--- src/main/java/com/example/tra/DTOs/ApplicantDTO.java
+++ src/main/java/com/example/tra/DTOs/ApplicantDTO.java
@@ -6,4 +6,11 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 public class ApplicantDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String nationality;
+    private String passportNumber;
+    private boolean criminalRecorde;
+
 }
