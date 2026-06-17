887b53a12ab584812924b34905fb6dc634078f54
Alanoud <alanood94895@gmail.com>
2026-06-17 10:59:36 +0400

    Create VisaApplicationDTO Class

--- src/main/java/com/example/tra/DTOs/VisaApplicationDTO.java
+++ src/main/java/com/example/tra/DTOs/VisaApplicationDTO.java
@@ -1,5 +1,6 @@
 package com.example.tra.DTOs;
 
+import com.example.tra.Entities.VisaApplication;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -12,4 +13,15 @@ public class VisaApplicationDTO {
     private String officerNotes;
     private Long applicantId;
     private Long officerId;
+
+
+    public static VisaApplicationDTO convertToDTO(VisaApplication visa) {
+        VisaApplicationDTO dto = new VisaApplicationDTO();
+        dto.setId(visa.getId());
+        dto.setVisaType(visa.getVisaType());
+        dto.setStatus(visa.getStatus());
+        dto.setOfficerNotes(visa.getOfficerNotes());
+        dto.setApplicantId(visa.getApplicant().getId());
+        dto.setOfficerId(visa.getHandlingOfficer().getId());
+        return dto;
 }
