d80675e5a599e8d9b6ef821160b1d3c27fa3e3ba
Alanoud <alanood94895@gmail.com>
2026-06-17 10:46:49 +0400

    Create CenterDTO Class

--- src/main/java/com/example/tra/DTOs/CenterDTO.java
+++ src/main/java/com/example/tra/DTOs/CenterDTO.java
@@ -1,5 +1,6 @@
 package com.example.tra.DTOs;
 
+import com.example.tra.Entities.ImmigrationCenter;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,4 +12,13 @@ public class CenterDTO {
     private String locationCountry;
     private String type;
     private int dailyCapacity;
+
+    public static CenterDTO convertToDTO(ImmigrationCenter center) {
+        CenterDTO dto = new CenterDTO();
+        dto.setId(center.getId());
+        dto.setName(center.getName());
+        dto.setLocationCountry(center.getLocationCountry());
+        dto.setType(center.getType());
+        dto.setDailyCapacity(center.getDailyCapacity());
+        return dto;
 }
